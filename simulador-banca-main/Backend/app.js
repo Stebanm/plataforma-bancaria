2ba75ea34eaca757026a5c37f7384a01ef2cf9a6
Stebanm <juanesxde16@gmail.com>
2024-09-06 17:51:57 -0500

    config cors

--- simulador-banca-main/Backend/app.js
+++ simulador-banca-main/Backend/app.js
@@ -8,8 +8,8 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: "https://plataforma-bancaria.vercel.app",
+    methods: "GET, HEAD ,PUT ,PATCH ,POST ,DELETE",
   })
 );
 

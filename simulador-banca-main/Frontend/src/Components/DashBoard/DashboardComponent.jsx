4bd93063df029b1240f9eb092b90f6f39ffbfaad
Stebanm <juanesxde16@gmail.com>
2024-10-09 06:54:23 -0500

    Delete: remove img sidebar

--- simulador-banca-main/Frontend/src/Components/DashBoard/DashboardComponent.jsx
+++ simulador-banca-main/Frontend/src/Components/DashBoard/DashboardComponent.jsx
@@ -372,75 +372,80 @@ export const DashboardComponent = () => {
                           <>
                             {/* Dropdown */}
 
-                            <div
-                              className="flex items-center px-4 py-2 font-medium tracking-wide text-darkGray capitalize transition-colors duration-300 transform bg-transparent rounded-md hover:bg-darkGray hover:text-white focus:outline-none space-x-2 w-full xl:text-sm 2xl:text-base"
+                            <button
+                              className="flex justify-between items-center px-4 py-2 font-medium tracking-wide text-darkGray capitalize transition-colors duration-300 transform bg-transparent rounded-md hover:bg-darkGray hover:text-white focus:outline-none space-x-2 w-full xl:text-sm 2xl:text-base"
                               onClick={toggleDropdown}
                             >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="size-5 xl:size-6"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-                                />
-                              </svg>
-
-                              <span className="mx-1">Cuenta de Ahorros</span>
-                              <button onClick={toggleDropdown}>
-                                {isOpen ? (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="size-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="m4.5 15.75 7.5-7.5 7.5 7.5"
-                                    />
-                                  </svg>
-                                ) : (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="size-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                                    />
-                                  </svg>
-                                )}
-                              </button>
-                            </div>
-                            {isOpen && (
-                              <div className="mt-2 space-y-2 ">
-                                <button
-                                  className="flex items-center px-4 py-2 font-medium tracking-wide text-darkGray capitalize transition-colors duration-300 transform bg-transparent rounded-md hover:bg-gray-100  hover:text-bg-darkGray focus:outline-none space-x-2 w-full xl:text-sm 2xl:text-base"
-                                  onClick={() => {
-                                    closeSidebar();
-                                    handleBotonClick(
-                                      "FormularioPersonaNatural"
-                                    );
-                                  }}
+                              <div className="flex flex-row justify-center items-center">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="size-5 xl:size-6"
                                 >
-                                  <span className="mx-1 text-center px-10">
-                                    Natural
-                                  </span>
-                                </button>
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+                                  />
+                                </svg>
+
+                                <span className="mx-1">Cuenta de Ahorros</span>
+                              </div>
+
+                              {isOpen ? (
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="size-4"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                                  />
+                                </svg>
+                              ) : (
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="size-4"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                                  />
+                                </svg>
+                              )}
+                            </button>
+                            {isOpen && (
+                              <div className="pl-7 relative overflow-hidden transform transition-transform duration-100 ease-in-out origin-top ">
+                                <div className="absolute left-6 top-0 bottom-0 w-px bg-gray-300" />
+
+                                <div className="flex flex-col justify-center items-center gap-y-1">
+                                  <button
+                                    className="flex items-center px-4 py-2 font-medium tracking-wide text-darkGray capitalize transition-colors duration-300 transform bg-transparent rounded-md hover:bg-gray-100  hover:text-bg-darkGray focus:outline-none space-x-2 w-full xl:text-sm 2xl:text-base"
+                                    onClick={() => {
+                                      closeSidebar();
+                                      handleBotonClick(
+                                        "FormularioPersonaNatural"
+                                      );
+                                    }}
+                                  >
+                                    <span className="mx-1 text-center px-10">
+                                      Natural
+                                    </span>
+                                  </button>
+                                </div>
                               </div>
                             )}
 
@@ -697,19 +702,6 @@ export const DashboardComponent = () => {
                         )}
                       </ul>
                       <hr className="border-gray-200 dark:border-gray-700 mt-6" />
-                    </div>
-
-                    <div className="mx-4">
-                      <div
-                        className="relative flex flex-col min-w-0 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border"
-                        sidenav-card="true"
-                      >
-                        <img
-                          className="w-1/1 mx-auto"
-                          src={Analytics}
-                          alt="sidebar illustrations"
-                        />
-                      </div>
                     </div>
                   </div>
                 </div>
@@ -1004,16 +996,6 @@ export const DashboardComponent = () => {
                         </button>
                       </ul>
                       <hr className="border-gray-200 dark:border-gray-700 mt-6" />
-                    </div>
-
-                    <div className="mx-4">
-                      <div className="relative flex flex-col min-w-0 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
-                        <img
-                          className="w-1/1 mx-auto"
-                          src="/src/assets/Img/UsoVario/Analytics.svg"
-                          alt="sidebar illustrations"
-                        />
-                      </div>
                     </div>
                   </div>
                 </div>
